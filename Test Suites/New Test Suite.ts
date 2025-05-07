74323d01533fd648b24be6088643c2afd1a67375
Hoang Ho <hoang.hho@ip-10-10-32-49.ap-southeast-1.compute.internal>
2025-05-07 18:17:51 +0700

    Add: Add test case

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -5,10 +5,24 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>03c658c6-f3a8-4ee8-877d-19907a0ccfda</testSuiteGuid>
+   <testCaseLink>
+      <guid>f083ee6b-f0d5-449d-b9a8-1611c01ac7ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ee5942cd-7d7d-44f5-9333-51bd767f080a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AI-Generated/TC1-Verify Hello Button Functionality and Page Navigation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
